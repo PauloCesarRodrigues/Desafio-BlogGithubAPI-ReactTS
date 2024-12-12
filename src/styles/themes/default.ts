46cfa95a4f37b2f15eb84e47d2a9b23b0c23db9f
Paulo Cesar Rodrigues <pauloceasrrtv321@gmail.com>
2024-12-12 11:11:21 -0300

    update: some little changes and colors change

--- src/styles/themes/default.ts
+++ src/styles/themes/default.ts
@@ -1,21 +1,16 @@
 export const defaultTheme = {
   white: '#FFF',
 
-  'gray-100': '#E1E1E6',
-  'gray-300': '#C4C4CC',
-  'gray-400': '#8D8D99',
-  'gray-500': '#7C7C8A',
-  'gray-600': '#323238',
-  'gray-700': '#29292E',
-  'gray-800': '#202024',
-  'gray-900': '#121214',
+  'darkblue-100': '#E75DF4', // Title
+  'darkblue-200': '#C4D4E3', // Subtitle
+  'darkblue-300': '#AF2C2D4', // Text
+  'darkblue-400': '#7B96B2', // Span
+  'darkblue-500': '#3A536B', // Label
+  'darkblue-600': '#1C2F41', // Border
+  'darkblue-700': '#112131', // Post
+  'darkblue-800': '#0B1B2B', // Profile
+  'darkblue-900': '#71422', // Background
 
-  'green-300': '#00B37E',
-  'green-500': '#00875F',
-  'green-700': '#015F43',
+  'blue-500': '#3294F8', // Brand
 
-  'red-500': '#AB222E',
-  'red-700': '#7A1921',
-
-  'yellow-500': '#FBA94C',
 }
